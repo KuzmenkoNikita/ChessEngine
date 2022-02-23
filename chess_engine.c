1dac9f72c25909bb7ac8384e82fe556a70701d5c
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2022-02-23 20:45:21 +0300

    Corrected desk array

--- chess_engine.c
+++ chess_engine.c
@@ -9,6 +9,7 @@
 typedef uint8_t game_cell;   
 
 #define GAME_DESK_LINE_SIZE 8
+#define PLAYER_MAX_FIGURES_COUNT    16
 
 /** @brief pieces types*/
 #define FIGURE_TYPE_KING    1 
@@ -45,6 +46,73 @@ typedef uint8_t game_cell;
 static unsigned int m_fig_weigh[FIGURES_TYPES_COUNT+1] = {0, FIGURE_TYPE_KING, FIGURE_TYPE_QUEEN,
                                                             FIGURE_TYPE_ROOK, FIGURE_TYPE_BISHOP,
                                                             FIGURE_TYPE_KNIGHT, FIGURE_TYPE_PAWN};
- /* ********************************* */
+/* ********************************* */
 /** @brief  game desk array */                                                           
-game_cell m_game_desk[GAME_DESK_LINE_SIZE][GAME_DESK_LINE_SIZE];
+game_cell m_game_desk[] = 
+{
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+    255, 255, 255, 255,      0,   0,   0,   0,  0,   0,   0,   0,       255, 255, 255, 255,
+
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+    255, 255, 255, 255,     255, 255, 255, 255,255, 255, 255, 255,      255, 255, 255, 255,
+};
+
+/* ********************************* */
+/** @brief  struct contains information about figure */  
+typedef struct figure_info_
+{
+    uint8_t figure_code;    
+    uint8_t x; 
+    uint8_t y;
+    struct figure_info_* p_next_fig; /* Pointer to next figure */
+}figure_info;
+
+/* ********************************* */
+/** @brief  white and black figures info */     
+figure_info m_w_figs_info[PLAYER_MAX_FIGURES_COUNT];
+figure_info m_b_figs_info[PLAYER_MAX_FIGURES_COUNT];
+
+/* ********************************* */
+/** @brief figure move descriptor */ 
+typedef struct 
+{
+    uint8_t figure_code;
+    uint8_t x1;
+    uint8_t x2;
+    uint8_t y1;
+    uint8_t y2;
+}move_desc;
+
+uint32_t engine_make_move(void)
+{
+
+}
+
+
+
+void chess_engine_init(void)
+{
+    for(uint8_t n = FIGURE_TYPE_PAWN; n >= FIGURE_TYPE_KING; n--)
+    {
+        for(uint8_t y = 0; y < GAME_DESK_LINE_SIZE; ++y)
+        {
+            for(uint8_t x = 0; x < GAME_DESK_LINE_SIZE; ++x)
+            {
+
+            }
+        }
+    }
+}
---
+++ chess_engine.h
@@ -0,0 +1,10 @@
+#ifndef _CHESS_ENGINE_
+#define _CHESS_ENGINE_
+
+void chess_engine_init(void);
+
+
+
+
+
+#endif /* _CHESS_ENGINE_ */
